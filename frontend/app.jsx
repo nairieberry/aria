24203e8cb2f461a24403fa892af1de2b659bfd09
nairieberry <wesleytam@yahoo.com>
2019-09-10 10:59:17 -0700

    auth stuff

---
+++ frontend/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignupContainer from './';
+import {Route} from 'react-router-dom';
+
+export default () => {
+    <div>
+        <Route path="/" component={} />
+    </div>
+};
