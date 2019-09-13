e475b1f3489333933b5c53b0269cd5254da1f282
nairieberry <wesleytam@yahoo.com>
2019-09-12 20:20:01 -0700

    added server utils and server actions

---
+++ frontend/actions/server.js
@@ -0,0 +1,26 @@
+import {fetchAllServers, fetchServer, createServer, updateServer} from '../utils/server';
+
+export const RECEIVE_ALL_SERVERS = 'RECEIVE_ALL_SERVERS'
+export const RECEIVE_CURRENT_SERVER = 'RECEIVE_CURRENT_SERVER'
+
+const receiveAllServers = servers => ({
+    type: RECEIVE_ALL_SERVERS,
+    servers
+});
+
+const receiveCurrentServer = server => ({
+    type: RECEIVE_SERVER,
+    server
+});
+
+export const serverIndex = () => dispatch => fetchAllServers()
+    .then(servers => dispatch(receiveAllServers(servers)));
+
+export const showServer = id => dispatch => fetchServer(id)
+    .then(server => dispatch(receiveCurrentServer(server)));
+
+export const newServer = server => dispatch => createServer(server)
+    .then(server => dispatch(receiveCurrentServer(server)));
+
+export const editServer = server => dispatch => updateServer(server)
+    .then(server => dispatch(receiveCurrentServer(server)));
---
+++ frontend/utils/server.js
@@ -0,0 +1,33 @@
+export const fetchAllServers = () => (
+    $.ajax({
+        method: 'GET',
+        url: '/api/servers'
+    })
+);
+
+export const fetchServer = (id) => (
+    $.ajax({
+        method: 'GET',
+        url: `/api/servers/${id}`
+    })
+);
+
+export const createServer = (server) => {
+        server.channels = Object.keys(server.channels).map(key => server.channels[key]);
+
+        return $.ajax({
+            method: 'POST',
+            url: 'api/servers',
+            data: {server}
+        });
+};
+
+export const updateServer = (server) => {
+    server.channels = Object.keys(server.channels).map(key => server.channels[key]);
+
+    return $.ajax({
+        method: 'PATCH',
+        url: `api/servers/${id}`,
+        data: {server}
+    });
+};
