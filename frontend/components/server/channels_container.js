0fe7bc15ec36fa4b9c0b5ab5f3fea8bd659d1cdb
nairieberry <wesleytam@yahoo.com>
2019-09-12 21:38:35 -0700

    attempted server components

---
+++ frontend/components/server/channels_container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import {showServer, newServer, editServer}
+
+const mapStateToProps = ({ errors }) => {
+    return {
+// do I need text here when I can just manually put in whatever into the jsx?
+// why am I feeding in errors?
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+    show: id => dispatch(showServer(id)),
+    new: server => dispatch(newServer(server)),
+    edit: server => dispatch(editServer(server)),
+// do I need these functions here? if so, which ones and why?
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(server.jsx file here?);
