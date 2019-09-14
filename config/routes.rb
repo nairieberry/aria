cacbfaecad25c2947f60cbc40b7d7d317eeb92ec
nairieberry <wesleytam@yahoo.com>
2019-09-14 14:52:19 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :servers, only: [:index, :show, :create, :update, :delete]
+    resources :channels, only: [:index, :show, :create, :delete]
+    resources :messages, only: [:index, :create, :edit, :delete]
   end
 
 end
