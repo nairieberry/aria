49b4549f37486ea43479839f61add31b51dbfcd4
nairieberry <wesleytam@yahoo.com>
2019-09-16 10:09:50 -0700

    message controller

--- app/controllers/api/message_controller.rb
+++ app/controllers/api/message_controller.rb
@@ -6,14 +6,32 @@ class Api::MessageController < ApplicationController
 
     def create
         @channel = Channel.new(channel_params)
+        @channel.owner_id = current_user.id
+        if @server.save
+            render :show
+        else
+            render json: @channel.errors.full_messages, status: 422
+        end
     end
 
     def update
+        @channel = current_user.channels.find(params[:id])
+        @channel.user_id = current_user.id
+        if @channel.update(channel_params)
+            render :show
+        else
+            render json: @channel.errors.full_messages, status: 422
+        end
     end
 
     def delete
+        @channel = current_user.channels.find(params[:id])
+        @channel.destroy!
+        render :show
     end
 
     def channel_params
+        params.require(:channel).permit(:channel_name, :description)
+    end
 
 end
